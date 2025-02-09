fc3f3d9a13fe5ac9658b1e381fda73167bae2aaa
liyakat ali <aliliyakat902@gmail.com>
2025-02-09 20:04:30 +0800

    All about variables.

---
+++ 01-variables.py
@@ -0,0 +1,31 @@
+# Variables are containers that store different types data
+
+can ="soda"     # string
+print(can)
+
+pizza = 100     # int
+print(pizza)
+
+rate = 20.5     # float
+print(rate)
+
+val = pizza > 50        # bool
+print(val)
+
+print(type(can))  # gives type of data the variable contans
+
+
+food = "jalebi"
+print(food)
+
+bar = food      # pointing same memory of food variable
+print(bar)
+
+print(id(food))
+print(id(bar))  # both same
+
+'''Rules for Naming variables
+        1:- Reserved words can't be used.
+        2:- Must start with a letter or underscore
+        3:- Can't contain spaces or special characters
+        '''
